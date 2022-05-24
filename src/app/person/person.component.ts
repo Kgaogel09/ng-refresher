086067736163c0f7eec3e42c3836019ea8136086
Kgaogel09 <kgaogelo@swipeix.com>
2022-05-24 17:34:48 +0200

    observables

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,16 +1,18 @@
 import { PersonService } from './person.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
 })
-export class PersonComponent implements OnInit {
+export class PersonComponent implements OnInit, OnDestroy {
   // private personService: PersonService;
   // @Input() personList: string[];
 
   // using services
   personList: string[];
+  private personListSub: Subscription;
 
   constructor(private prsService: PersonService) {
     // this.personService = prsService;
@@ -18,5 +20,15 @@ export class PersonComponent implements OnInit {
 
   ngOnInit() {
     this.personList = this.prsService.persons;
+    this.personListSub = this.prsService.updatedPerson.subscribe((persons) => {
+      this.personList = persons;
+    });
+  }
+
+  onRemovePerson(personName: string) {
+    this.prsService.removePerson(personName);
+  }
+  ngOnDestroy(): void {
+    this.personListSub.unsubscribe();
   }
 }
--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PersonService {
+  updatedPerson = new Subject<string[]>();
   persons: string[] = ['Max', 'Manuel', 'john'];
   addPerson(name: string) {
     this.persons.push(name);
-    console.log(this.persons);
+    this.updatedPerson.next(this.persons);
+  }
+  removePerson(name: string) {
+    this.persons = this.persons.filter((person) => {
+      return person !== name;
+    });
+    this.updatedPerson.next(this.persons);
   }
 }
